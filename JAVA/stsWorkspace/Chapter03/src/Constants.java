7997bf15cfe36beab8431ef73966eb3f2cb6dc3d
CheolEon(KOSMO) <kchy1234@naver.com>
2018-06-21 15:08:58 +0900

    encoding commi

--- JAVA/stsWorkspace/Chapter03/src/Constants.java
+++ JAVA/stsWorkspace/Chapter03/src/Constants.java
@@ -8,10 +8,11 @@ public class Constants {
 		final int CONST_ASSIGNED;
 		
 		CONST_ASSIGNED = 12;
-//		CONST_ASSIGNED = 14; 재 할당 불가하다는 소리
+//		CONST_ASSIGNED = 14; 재 할당 불가
 		
 		System.out.println("상수1 : " + MAX_SIZE);
 		System.out.println("상수2 : " + CONST_CHAR);
 		System.out.println("상수3 : " + CONST_ASSIGNED);
 	}
+
 }
---
+++ JAVA/stsWorkspace/Chapter03/src/PrefixOp.java
@@ -0,0 +1,22 @@
+
+public class PrefixOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 7;
+		System.out.println("전위 증감연산");
+		System.out.println("++num : " + ++num);
+		System.out.println("++num : " + ++num);
+		System.out.println("num : " + num);
+		
+		System.out.println("----------------");
+		
+		System.out.println("후위 증감연산");
+		System.out.println("num++ : " + num++);
+		System.out.println("num++ : " + num++);
+		System.out.println("num : " + num);
+
+	}
+
+}
