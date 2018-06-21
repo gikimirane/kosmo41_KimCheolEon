a16fe3ef477cb2c28f43b9b29f593515f4c09ab6
CheolEon(KOSMO) <kchy1234@naver.com>
2018-06-21 18:55:11 +0900

    KOSMO; 180621 end

--- JAVA/stsWorkspace/Chapter05/src/InputNum5.java
+++ JAVA/stsWorkspace/Chapter05/src/InputNum5.java
@@ -9,21 +9,28 @@ public class InputNum5 {
 		
 		System.out.println("문제5. 계속해서 정수 입력받고, 0입력시 모든 정수 더한값 출력");
 				
+//		int buffer = 0;
+//		int i = 0;
+//		while(i < 9999999) {
+//			
+//			System.out.println("숫자를 입력하세요.");
+//			int num1 = scan.nextInt();
+//			
+//			buffer = buffer + num1;
+//			
+//			if(num1 == 0) {
+//				System.out.println("0 입력, 정지");
+//				break;
+//			}
+//			i++;			
+//		}
 		int buffer = 0;
-		int i = 0;
-		while(i < 9999999) {
-			
+		int num1 = 0;
+		do {
 			System.out.println("숫자를 입력하세요.");
-			int num1 = scan.nextInt();
-			
+			num1 = scan.nextInt();
 			buffer = buffer + num1;
-			
-			if(num1 == 0) {
-				System.out.println("0 입력, 정지");
-				break;
-			}
-			i++;			
-		}
+		}while(num1 != 0);
 		
 		System.out.println("전체 입력값 출력 : " + buffer);
 	}
