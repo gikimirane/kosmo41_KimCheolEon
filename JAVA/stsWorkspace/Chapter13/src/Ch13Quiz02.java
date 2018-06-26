f06949ae3f867c0c4237ce051d5bcaff0a359a25
KCE1070 <kchy12345@gmail.com>
2018-06-26 22:12:37 +0900

    HOME; Ch13Quiz02 + Ch13Quiz05; Adjust Complete;

--- JAVA/stsWorkspace/Chapter13/src/Ch13Quiz02.java
+++ JAVA/stsWorkspace/Chapter13/src/Ch13Quiz02.java
@@ -8,9 +8,12 @@ public class Ch13Quiz02 {
 		
 //		String b = new String(a); 이건 참고 넣는 방법
 		//아 조건이 char 조건이었네...
-		String[] sArray = new String[] {"Good time"};
+//		String[] sArray = new String[] {"Good time"};
+		
+		char[] sArray = new char[] {'G', 'o', 'o', 'd', ' ', 'T', 'i', 'm', 'e'};
 //		
 //		System.out.println(sArray[0]);
+		System.out.println(sArray);
 	
 	}
 
--- JAVA/stsWorkspace/Chapter13/src/Ch13Quiz05.java
+++ JAVA/stsWorkspace/Chapter13/src/Ch13Quiz05.java
@@ -16,39 +16,51 @@ public class Ch13Quiz05 {
 		String[] array_word = new String[word.length()]; // 스트링을 담을 배열 선언 및 크기 지정
 		String[] bufferArray = new String[word.length()];
 
+		int nCount = 0;
+
 		for (int i = 0, j = array_word.length; i < array_word.length; i++, j--) { // 스트링을 한글자씩 끊어 배열에 저장
 			array_word[i] = Character.toString(word.charAt(i));
-			bufferArray[j-1] = Character.toString(word.charAt(i));
+			bufferArray[j - 1] = Character.toString(word.charAt(i));
 		}
-		
-		
-//		for(int i = 0, j = array_word.length; i < array_word.length/2; i++, j--) {
-//			if(array_word[i] == )
-////				한 단어씩 앞뒤에서 비교하려고했는데 일단 보류
-//		}
+
+		// for(int i = 0, j = array_word.length; i < array_word.length/2; i++, j--) {
+		// if(array_word[i] == )
+		//// 한 단어씩 앞뒤에서 비교하려고했는데 일단 보류
+		// }
 
 		// 출력 테스트용… 실제 사용시 안써도 됨.
 		for (int i = 0; i < array_word.length; i++) {
-//			System.out.println(array_word[i]);
+			// System.out.println(array_word[i]);
 			System.out.print(array_word[i]);
 		}
 		System.out.println();
-		//버퍼 출력 테스트
+		// 버퍼 출력 테스트
 		for (int i = 0; i < array_word.length; i++) {
-//			System.out.println(array_word[i]);
+			// System.out.println(array_word[i]);
 			System.out.print(bufferArray[i]);
 		}
-		
+
 		System.out.println();
-		
+
 		System.out.println("원본 : " + Arrays.toString(array_word));
 		System.out.println("버퍼 : " + Arrays.toString(bufferArray));
 
-		if(array_word.equals(bufferArray) == true) {
-			System.out.println("회문입니다");
+		// 테스트 결과, 정상
+		// boolean buf = array_word[0].equals(bufferArray[0]);
+		// System.out.println(buf);
+		boolean Judgment = true;
+		for (int i = 0; i < (array_word.length / 2); i++) {
+			if (array_word[i].equals(bufferArray[i]) == true) {
+				Judgment = true;
+			} else {
+				Judgment = false;
+			}
 		}
-		else if(array_word.equals(bufferArray) == false) {
-			System.out.println("회문이 아닙니다");
+
+		if (Judgment == true) {
+			System.out.println("회문입니다");
+		} else {
+			System.out.println("회문이 아닙니다.");
 		}
 
 	}
