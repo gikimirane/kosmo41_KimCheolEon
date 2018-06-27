dea0252f6bab6df317f3b1c50d86b54caa683130
kchy12345 <kchy12345@gmail.com>
2018-06-27 09:29:37 +0900

    KOSMO; Ch13Quiz05 error adjust; ignore change;

--- JAVA/stsWorkspace/TestProject/src/Ch13Quiz05.java
+++ JAVA/stsWorkspace/TestProject/src/Ch13Quiz05.java
@@ -1,5 +1,3 @@
-//회문 탐색할 때 false면 바로 break 걸고 나와서 아니라고 뱉어내야함
-
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -18,10 +16,17 @@ public class Ch13Quiz05 {
 		String[] array_word = new String[word.length()]; // 스트링을 담을 배열 선언 및 크기 지정
 		String[] bufferArray = new String[word.length()];
 
+		int nCount = 0;
+
 		for (int i = 0, j = array_word.length; i < array_word.length; i++, j--) { // 스트링을 한글자씩 끊어 배열에 저장
 			array_word[i] = Character.toString(word.charAt(i));
 			bufferArray[j - 1] = Character.toString(word.charAt(i));
 		}
+
+		// for(int i = 0, j = array_word.length; i < array_word.length/2; i++, j--) {
+		// if(array_word[i] == )
+		//// 한 단어씩 앞뒤에서 비교하려고했는데 일단 보류
+		// }
 
 		// 출력 테스트용… 실제 사용시 안써도 됨.
 		for (int i = 0; i < array_word.length; i++) {
@@ -43,13 +48,12 @@ public class Ch13Quiz05 {
 		// 테스트 결과, 정상
 		// boolean buf = array_word[0].equals(bufferArray[0]);
 		// System.out.println(buf);
-		boolean Judgment = false;
-		for (int i = 0; i < array_word.length; i++) {
+		boolean Judgment = true;
+		for (int i = 0; i < (array_word.length / 2); i++) {
 			if (array_word[i].equals(bufferArray[i]) == true) {
 				Judgment = true;
 			} else {
 				Judgment = false;
-				break;
 			}
 		}
 
@@ -58,5 +62,7 @@ public class Ch13Quiz05 {
 		} else {
 			System.out.println("회문이 아닙니다.");
 		}
+
 	}
+
 }
